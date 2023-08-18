d569c361a96705fdd0cdc27b4b53130608d8cee2
EduardoGlez16 <105452946+EduardoGlez16@users.noreply.github.com>
2023-08-18 16:42:35 -0600

    se agrego rn

--- src/main/java/com/imss/sivimss/promotores/service/impl/ResgistrarActividadImpl.java
+++ src/main/java/com/imss/sivimss/promotores/service/impl/ResgistrarActividadImpl.java
@@ -75,7 +75,6 @@ public class ResgistrarActividadImpl implements RegistrarActividadService {
 	
 	@Override
 	public Response<?> buscarFormatoActividades(DatosRequest request, Authentication authentication) throws IOException {
-		  Response<?> response = new Response<>();
 		String datosJson = String.valueOf(request.getDatos().get("datos"));
 		FiltrosPromotorActividadesRequest filtros = gson.fromJson(datosJson, FiltrosPromotorActividadesRequest.class);
 		 Integer pagina = Integer.valueOf(Integer.parseInt(request.getDatos().get("pagina").toString()));
@@ -83,17 +82,16 @@ public class ResgistrarActividadImpl implements RegistrarActividadService {
 	        filtros.setTamanio(tamanio.toString());
 	        filtros.setPagina(pagina.toString());
 	    	UsuarioDto usuario = gson.fromJson((String) authentication.getPrincipal(), UsuarioDto.class);
-	        response = providerRestTemplate.consumirServicio(registrarActividad.buscarFormatoActividades(request, filtros, fecFormat).getDatos(), urlPaginado,
+	    	Response<?> response = providerRestTemplate.consumirServicio(registrarActividad.buscarFormatoActividades(request, filtros, fecFormat).getDatos(), urlPaginado,
 				authentication);
 	        if(response.getDatos().toString().contains("id")) {
 	        	logUtil.crearArchivoLog(Level.INFO.toString(), this.getClass().getSimpleName(),this.getClass().getPackage().toString(),"CONSULTA FORMATO REGISTRO DE ACTIVIDADES OK", CONSULTA, authentication, usuario);
 	        }else {
 	        	response.setError(true);
 	        	response.setMensaje("45");
-	        }
+	        	response.setDatos(null);
+	        } 
 	    	return response;
-	        	
-	        
 	}
 
 
